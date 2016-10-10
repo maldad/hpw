cd26da800f4c60e8dfd95c12b36618353bec547a
maldad <canond_7@hotmail.com>
2016-10-10 13:37:44 -0500

    all tests unless the double space "  "

--- qunit/js/rot13.js
+++ qunit/js/rot13.js
@@ -6,7 +6,7 @@ var Rot13 = (function () {
         a: 'n', b: 'o', c: 'p', d: 'q', e: 'r', f: 's', g: 't', h: 'u', i: 'v',
         j: 'w', k: 'x', l: 'y', m: 'z', n: 'a', o: 'b', p: 'c', q: 'd', r: 'e',
         s: 'f', t: 'g', u: 'h', v: 'i', w: 'j', x: 'k', y: 'l', z: 'm',
-        ' ': ' '
+        ' ': ' ', '\n': '\n', '\t': '\t'
     };
 
     var change = function(character) {
@@ -15,8 +15,14 @@ var Rot13 = (function () {
 
     var encrypt = function(string) {
         var output = "";
-        for(var i = 0; i < string.length; i++)
-            output = output + change(string[i]);
+        for(var i = 0; i < string.length; i++) { 
+            var c = change(string[i]);
+            if(typeof c === 'undefined')
+                return ''; 
+            if(typeof c === 'number')
+                return ''; 
+            output = output + c;
+        }
         return output;
     }
     
--- qunit/js/tests/rot13.js
+++ qunit/js/tests/rot13.js
@@ -20,6 +20,13 @@ QUnit.test("Tests de Rot13", function(assert) {
         "z",
         "m por z"
     );
+
+    assert.strictEqual(
+        Rot13.encrypt("H"),
+        "U",
+        "H por U"
+    );
+
     assert.strictEqual(
         Rot13.encrypt("HOLA"),
         "UBYN",
@@ -27,8 +34,63 @@ QUnit.test("Tests de Rot13", function(assert) {
     );
 
     assert.strictEqual(
-        Rot13.encrypt("H"),
-        "U",
-        "H por U"
+        Rot13.encrypt("Hola"),
+        "Ubyn",
+        "Hola por Ubyn"
     );
+   
+    assert.strictEqual(
+        Rot13.encrypt("hOLA"),
+        "uBYN",
+        "hOLA por uBYN"
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt("hOlA"),
+        "uByN",
+        "hOlA por uByN"
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt(" h o l a"),
+        " u b y n",
+        " h o l a por  u b y n"
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt("  "),
+        "  ",
+        "   por   "
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt("HOLÑ"),
+        "",
+        'HOLÑ por ""'
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt("HO1A"),
+        "",
+        'HO1A por ""'
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt("HO@A"),
+        "",
+        'HO@A por ""'
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt("HO\nLA"),
+        "UB\nYN",
+        'HO\nLA por UB\nYN'
+    );
+
+    assert.strictEqual(
+        Rot13.encrypt("HO\tLA"),
+        "UB\tYN",
+        'HO\tLA por UB\tYN'
+    );
+
 });
