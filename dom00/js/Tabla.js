326c790ea3e9a3f4c548af912b16a18cf828d045
maldad <canond_7@hotmail.com>
2016-10-19 15:02:29 -0500

    diagonal abajo, derecha e izquierda

--- dom00/js/Tabla.js
+++ dom00/js/Tabla.js
@@ -18,9 +18,19 @@ var Tabla = (function (){
     var diagonal_abajo_derecha = function(obj_tabla, fila, columna){
         var arr_salida = [];
         var j = columna;
-        for(var i = fila - 1; i < obj_tabla.children[1].rows.length; i++){
+        for(var i = fila - 1; i < obj_tabla.children[1].rows.length && j <= obj_tabla.children[1].rows.length; i++){
             arr_salida.push(obj_tabla.children[1].rows[i].children[j]);
             j++;
+        }     
+        return arr_salida;
+    }
+
+    var diagonal_abajo_izquierda = function(obj_tabla, fila, columna){
+        var arr_salida = [];
+        var j = columna;
+        for(var i = fila - 1; i < obj_tabla.children[1].rows.length  && j > 0; i++){
+            arr_salida.push(obj_tabla.children[1].rows[i].children[j]);
+            j--;
         }     
         return arr_salida;
     }
@@ -28,6 +38,7 @@ var Tabla = (function (){
     return {
         "fila": fila,
         "columna": columna,
-        "diagonal_abajo_derecha": diagonal_abajo_derecha
+        "diagonal_abajo_derecha": diagonal_abajo_derecha,
+        "diagonal_abajo_izquierda": diagonal_abajo_izquierda
     }
 })();
