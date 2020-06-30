9a457d0a2d3bae5140106656891b3546687a4658
Agustin Vasquez Martinez <agustin@logicalbricks.com>
2020-06-30 17:56:34 -0500

    - [x] React utiliza funciones PURAS. - [x] Los scrips que mandan a llamar a React deben ir hasta el final de   la página, de otra forma no van a encontrar el elemento adecuado en el   DOM ya que no ha sido creado.

---
+++ react/js/react_examples.js
@@ -0,0 +1,5 @@
+// los elementos en React son inmutables.
+const element = <h1>Hello, world</h1>;
+ReactDOM.render(element, document.getElementById('root'));
+
+// All React components must act like pure functions with respect to their props.
